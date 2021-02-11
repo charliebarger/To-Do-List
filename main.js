93f419ae5b1d09d710798bb493c9512dc4aaba40
charliebarger <charliebarger04@gmail.com>
2021-02-11 08:41:11 -0700

    added event listner that adds active classes to slide in nav and hamburger icon

--- main.js
+++ main.js
@@ -2,6 +2,17 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+let burger = document.querySelector('.hamburger');
+let burgerClasses = burger.classList;
 
+let navBar = document.querySelector('nav');
+let navBarClasses = navBar.classList;
+
+
+burger.addEventListener('click', ()   => {
+  burgerClasses.toggle('closed')
+  navBarClasses.toggle('active')
+})
 /******/ })()
 ;
+//# sourceMappingURL=main.js.map
