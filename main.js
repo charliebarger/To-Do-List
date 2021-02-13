ad52d1455293413675a31f4582b1482a71387771
charliebarger <charliebarger04@gmail.com>
2021-02-13 14:13:44 -0700

    added funtions to add effects to project folders on click and hover

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+//burger icon//
 let burger = document.querySelector('.hamburger');
 let burgerClasses = burger.classList;
 
@@ -13,6 +14,33 @@ burger.addEventListener('click', ()   => {
   burgerClasses.toggle('closed')
   navBarClasses.toggle('active')
 })
+
+//hover and click project//
+
+let projectFolders = document.querySelectorAll('.project-folder')
+
+projectFolders.forEach(folder => {
+  folder.addEventListener('click', () => {
+    projectFolders.forEach(folder => {
+      if(folder.classList.contains('selected-project')){
+        folder.classList.remove('selected-project');
+        folder.lastElementChild.classList.remove('show-x')
+      }
+    })
+    folder.classList.add('selected-project')
+    folder.lastElementChild.classList.add('show-x')
+  })
+  folder.addEventListener('mouseover', () => {
+    folder.lastElementChild.classList.add('hover-x')
+  })
+  folder.addEventListener('mouseleave', () => {
+    folder.lastElementChild.classList.remove('hover-x')
+  })
+})
+
+//add new project//
+
+
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
