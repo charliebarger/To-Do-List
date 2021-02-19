9e876f77f03ec480bbcdfce93317d6647e779642
charliebarger <charliebarger04@gmail.com>
2021-02-19 08:55:50 -0700

    added function to append items in local storage on page laod

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import {addMenuEvent} from "./burgerMenu"
 import {updateFolders} from "./Projectfolders"
 import {callAddProjectListner} from "./projectTextfield"
+import {getStorageItems} from "./localStorage"
 //burger icon//
 addMenuEvent()
+getStorageItems()
 //hover and click project//
 updateFolders()
 callAddProjectListner()
--- src/localStorage.js
+++ src/localStorage.js
@@ -1,4 +1,6 @@
-let folderTasks = {}
+import {createSelectedFolder, createNotSelectedFolder} from "./createNewProject"
+
+let folderTasks = {"default": []}
 
 function addNewFolder(folderName) {
     folderTasks[folderName] = []
@@ -10,11 +12,26 @@ function addNewTasks(folderName, tasks){
     setLocalStorage()
 }
 
-export {addNewFolder, addNewTasks}
-
 function setLocalStorage(){
     for (let key in folderTasks){
         localStorage.setItem(JSON.stringify(key), JSON.stringify(folderTasks[key]))
     }
     console.log(localStorage)
 }
+
+function getStorageItems() {
+  setLocalStorage()
+  for (var i = localStorage.length -1; -1 < i; i--) {
+    let project = localStorage.key(i);
+    if(i == localStorage.length -1){
+        createSelectedFolder(JSON.parse(project))
+    }
+    else{
+        createNotSelectedFolder(JSON.parse(project))
+    }
+    // let tasks = JSON.parse(localStorage.getItem(key))
+  }
+}
+
+
+export {addNewFolder, addNewTasks, getStorageItems}
